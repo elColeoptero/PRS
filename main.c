ccbee5feb2b80b03cd22f68b6fb8ea4b626f83d2
MatthPolan <matthieu.polan@hotmail.fr>
2021-04-10 19:00:25 +0200

    Init joueur1

--- main.c
+++ main.c
@@ -11,19 +11,29 @@
 #include <stdio.h>
 #include <semaphore.h>
 #include <fcntl.h>
+#include <stdint.h>
+typedef struct  BoardDescription BoardDescription;
 void displayShmInfo(int Id);
 int nattch(int Id);
+struct BoardDescription
+{
+    uint64_t boards[2][6];
+    int half_moves;
+    uint8_t enPassant;
+    uint8_t rook;
+};
 void signalHandler(int signalNum) ;
 int couleur;
 sem_t *gameAccess;
-
+pid_t* playersIndex;
+BoardDescription* theGame;
 int main(int argc, char const *argv[]){
 	signal(SIGUSR1,signalHandler);	
 	printf("mon pid : %d",getpid());
- 	int shmid;
+ 	int shmid,shmid_partie;
 	key_t key;
 	key = ftok("/usr", 'R');
-	pid_t* playersIndex;
+
 	shmid = shmget (key, 2*sizeof(pid_t) , 0666 | IPC_CREAT | IPC_EXCL);
 	int errsv = errno;
 	switch(errsv){
@@ -38,11 +48,13 @@ int main(int argc, char const *argv[]){
 		if((nattch(shmid))==1)
 		{
 			playersIndex = shmat(shmid, NULL, 0);
+			shmid_partie = shmget (key+2, sizeof(BoardDescription) , 0666);
+			theGame  =  shmat(shmid_partie, NULL, 0);
+			gameAccess = sem_open ("gameAccess",0); 
 			printf("Vous allez rejoindre la partie\n");
 			couleur = 1;
 			playersIndex[1]=getpid();
-			gameAccess = sem_open ("gameAccess", O_CREAT, 0666, 1); 
-			sem_unlink ("gameAccess"); 
+			
 			printf("J'inite la partie hehe\n");
 			printf("J'affiche la partie hehe\n");
 			kill(playersIndex[0],SIGUSR1);
@@ -52,6 +64,10 @@ int main(int argc, char const *argv[]){
 		
 	case 0 :
 		playersIndex = shmat(shmid, NULL, 0);
+		shmid_partie = shmget (key+2, sizeof(BoardDescription) , 0666 | IPC_CREAT);
+		theGame  =  shmat(shmid_partie, NULL, 0);
+		gameAccess = sem_open ("gameAccess", O_CREAT, 0666, 1); 
+		sem_unlink ("gameAccess"); 
 		printf("Creation partie\nVous êtes le premier joueur...\n");
 		couleur = 0;
 		playersIndex[0]=getpid();
@@ -61,16 +77,14 @@ int main(int argc, char const *argv[]){
 		printf("tchao bye bye\n");
 		exit(-1);
 	}
-	
-	//while(1){
+	printf("%d",gameAccess);
+	while(1){
 		pause();
-		printf("COUCOU SAMUEL");
-	//}
+	}
 }
 
 int nattch(int Id)
 {
-
 	struct shmid_ds info;
 	shmctl(Id,IPC_STAT,&info);
 	return info.shm_nattch;
@@ -78,7 +92,10 @@ int nattch(int Id)
 
 void signalHandler(int signalNum) {
     printf("Caught SIGUSR1 signal.\n");
-    pause();
+    printf("couleur = %d\n",couleur); 
+    printf("playerindex = %d\n",playersIndex[(couleur+1)%2]);
+    sleep(3);
+    kill(playersIndex[(couleur+1)%2],SIGUSR1);
 }
 
 
