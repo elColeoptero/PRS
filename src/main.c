e2b0edb2d2d2a41cd2376930ca0f01f860f6a3be
MatthPolan <matthieu.polan@hotmail.fr>
2021-04-16 14:14:04 +0200

    cartouche + main.h

--- src/main.c
+++ src/main.c
@@ -1,41 +1,183 @@
-#include "chess_game.h"
-#include <stdio.h>
+/*******************************************************
+Nom ......... : main.c
+Role ........ : Permet de lancer ou de rejoindre une partie
+Auteur ...... : Saint-Omer/POLAN
 
-/*
-    boards[0][0] = 0b0000000000000000000000000000000000000000000000001111111100000000;
-    boards[0][1] = 0b0000000000000000000000000000000000000000000000000000000001000010;
-    boards[0][2] = 0b0000000000000000000000000000000000000000000000000000000000100100;
-    boards[0][3] = 0b0000000000000000000000000000000000000000000000000000000010000001;
-    boards[0][4] = 0b0000000000000000000000000000000000000000000000000000000000001000;
-    boards[0][5] = 0b0000000000000000000000000000000000000000000000000000000000010000;
+********************************************************/
+#include "main.h"
 
-    boards[1][0] = 0b0000000011111111000000000000000000000000000000000000000000000000;
-    boards[1][1] = 0b0100001000000000000000000000000000000000000000000000000000000000;
-    boards[1][2] = 0b0010010000000000000000000000000000000000000000000000000000000000;
-    boards[1][3] = 0b1000000100000000000000000000000000000000000000000000000000000000;
-    boards[1][4] = 0b0001000000000000000000000000000000000000000000000000000000000000;
-    boards[1][5] = 0b0000100000000000000000000000000000000000000000000000000000000000;
+//Global Var
+sem_t *gameAccess;
+InfoPlayers *thePlayers;
+BoardDescription *theGame;
 
-*/
+int main(int argc, char const *argv[])
+{
+    int shmid;
+    key_t key;
+    key = ftok("/usr", 'R');
+    newact.sa_handler = signalHandler;
+    newact.sa_flags = 0;
+    sigaction(SIGINT, &newact, NULL);
+    sigaction(SIGUSR1, &newact, NULL);
+    sigaction(SIGUSR2, &newact, NULL);
+    system("clear");
+    shmid = shmget(key, 2 * sizeof(pid_t), 0666 | IPC_CREAT | IPC_EXCL);
+    switch (errno)
+    {
+    case 17: //shm already exist
+        shmid = shmget(key, 2 * sizeof(pid_t), 0666);
+        if ((nattch(shmid)) > 1)
+        {
+            printf("trop de joueur déso\n");
+            exit(1);
+        }
 
+        if ((nattch(shmid)) == 1)
+        {
+            initVar(1, shmid, key);
+            if (argc > 1)
+                strncpy(thePlayers->namePlayer[1], argv[1], 20);
+            else
+                strncpy(thePlayers->namePlayer[1], "Annonyme", 20);
 
-int main(){
-    BoardDescription *boardDescription = malloc(sizeof(BoardDescription));
+            printf("Vous allez rejoindre la partie. \nMerci de patienter pour le premier coup de votre adversaire \n");
+            kill(thePlayers->playersIndex[0], SIGUSR1);
+            break;
+        }
 
-    initBoard(boardDescription);
+    case 0:
+        printf("Creation partie. Vous êtes le premier joueur...\n");
+        initVar(0, shmid, key);
+        if (argc > 1)
+            strncpy(thePlayers->namePlayer[0], argv[1], 20);
+        else
+            strncpy(thePlayers->namePlayer[0], "Annonyme", 20);
 
-    displayGame(boardDescription,0);
-    printf("%d\n",jouerCoup(boardDescription,"e2","e4",0));
-    displayGame(boardDescription,0);
-    printf("%d\n",jouerCoup(boardDescription,"e7","e5",1));
-    displayGame(boardDescription,0);
+        break;
 
-    printf("%d\n",jouerCoup(boardDescription,"g1","f3",0));
-    displayGame(boardDescription,0);
-    printf("%d\n",jouerCoup(boardDescription,"b8","c6",1));
-
-
-    displayGame(boardDescription,0);
-
+    default:
+        printf("Error\n");
+        exit(-1);
+    }
+    while (1)
+    {
+        pause();
+    }
 }
 
+/*******************************************************
+Nom ......... : initVar
+Role ........ : Permet d'initialiser les zones mémoires et le sémaphore
+                pour les 2 joueurs
+Arg ......... : color_conf : Couleur du joueur
+                shmid      : Id mémoire partagée
+                key        : Clé de la zone mémoire partagée
+Return....... : VOID
+********************************************************/
+
+void initVar(int color_conf, int shmid, int key)
+{
+    int shmid_partie;
+    thePlayers = shmat(shmid, NULL, 0);
+    if (color_conf == 0)
+    {
+        shmid_partie = shmget(key + 2, sizeof(BoardDescription), 0666 | IPC_CREAT);
+        gameAccess = sem_open("gameAccess", O_CREAT, 0666, 1);
+    }
+    else
+    {
+        shmid_partie = shmget(key + 2, sizeof(BoardDescription), 0666);
+        gameAccess = sem_open("gameAccess", 0);
+    }
+    theGame = shmat(shmid_partie, NULL, 0);
+    if (color_conf == 0)
+        initBoard(theGame);
+    else
+    {
+        init_bitboard();
+    }
+    thePlayers->playersIndex[color_conf] = getpid();
+    color = color_conf;
+}
+
+/*******************************************************
+Nom ......... : nattch
+Role ........ : Permet de récupèrer le nombre de processus attaché à une zone mémoire
+Arg ......... : Id : Id zone mémoire
+Return....... : Nombre de processus attachés à cette zone mémoire
+********************************************************/
+int nattch(int Id)
+{
+    struct shmid_ds info;
+    shmctl(Id, IPC_STAT, &info);
+    return info.shm_nattch;
+}
+
+/*******************************************************
+Nom ......... : signalHandler
+Role ........ : En fonction du signal recu par le processus 
+                lance un déroulement différent 
+Arg ......... : Id : numero du signal recu
+                    SIGINT : Fin de processus 
+                    SIGUSR1: Jouer une coup et afficher le board
+                    SIGUSR2: Fin du deuxieme joueur
+Return....... : void
+********************************************************/
+void signalHandler(int signalNum)
+{
+    int returnjouerCoup = 0;
+    char DestMove[3];
+    char SrcMove[3];
+
+    switch (signalNum)
+    {
+    case SIGINT:
+        puts("\n\nVous abandonnez la partie. \nDefaite ! ");
+        kill(thePlayers->playersIndex[(color + 1) % 2], SIGUSR2);
+        sem_close(gameAccess);
+        exit(1);
+        break;
+
+    case SIGUSR1:
+        system("clear");
+        sem_wait(gameAccess);
+        displayGame(theGame, color, thePlayers->namePlayer);
+        sem_post(gameAccess);
+
+        sem_wait(gameAccess);
+        /*if(gameFinish())
+	    	exit(1);
+	    */
+        sem_post(gameAccess);
+
+        sem_wait(gameAccess);
+        do
+        {
+            printf("Jouez une coup sous la forme : source-destination \n");
+            lire(SrcMove, 3);
+            lire(DestMove, 3);
+            returnjouerCoup = jouerCoup(theGame, SrcMove, DestMove, color);
+            //saisiUser();
+            if (returnjouerCoup == 2)
+                promotion(theGame, DestMove, color, 4);
+        } while (returnjouerCoup == 0);
+        sem_post(gameAccess);
+        system("clear");
+        sem_wait(gameAccess);
+        displayGame(theGame, color, thePlayers->namePlayer);
+        /*if(gameFinish())
+	    	exit(1);
+	    */
+        sem_post(gameAccess);
+        kill(thePlayers->playersIndex[(color + 1) % 2], SIGUSR1);
+        puts("Votre adversaire joue...");
+        break;
+    case SIGUSR2:
+        puts("\n\nVotre adversaire a décidé de quitter la partie vous gagnez par forfait. \nVictoire ! ");
+        sem_close(gameAccess);
+        sem_unlink("gameAccess");
+        exit(1);
+        break;
+    }
+}
---
+++ src/main.h
@@ -0,0 +1,29 @@
+#include <sys/shm.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <wait.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "lib/read_lib/read_lib.h"
+#include "lib/chess_game/chess_game.h"
+#include <stdlib.h>
+
+typedef struct InfoPlayers InfoPlayers;
+struct InfoPlayers
+{
+    char namePlayer[2][20];
+    pid_t playersIndex[2];
+};
+struct sigaction newact;
+void displayShmInfo(int Id);
+int nattch(int Id);
+void signalHandler(int signalNum);
+void initVar(int color_conf, int shmid, int key);
+int color;
